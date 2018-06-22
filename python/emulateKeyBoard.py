bc51fa584795804c62d94d682c4ecfb7064c578f
Patrick_Via_Raspberry <raspberry@ph-f.nl>
2018-06-22 14:18:34 +0200

    Change keys, listen to  QWER & 123

--- python/emulateKeyBoard.py
+++ python/emulateKeyBoard.py
@@ -3,37 +3,60 @@ import time
 from evdev import UInput, ecodes as e
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(5, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(6, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(19, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(20, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(21, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 ui = UInput()
 
-def button18(channel):
-    ui.write(e.EV_KEY, e.KEY_LEFT ,1)
-    ui.write(e.EV_KEY, e.KEY_LEFT ,0)
+def button5(channel):
+    ui.write(e.EV_KEY, e.KEY_Q ,1)
+    ui.write(e.EV_KEY, e.KEY_Q ,0)
     ui.syn()
 
-def button17(channel):
-    ui.write(e.EV_KEY, e.KEY_DOWN ,1)
-    ui.write(e.EV_KEY, e.KEY_DOWN ,0)
-    ui.syn()
-
-def button22(channel):
-    ui.write(e.EV_KEY, e.KEY_UP ,1)
-    ui.write(e.EV_KEY, e.KEY_UP ,0)
+def button6(channel):
+    ui.write(e.EV_KEY, e.KEY_W ,1)
+    ui.write(e.EV_KEY, e.KEY_W ,0)
     ui.syn()
 
 def button13(channel):
-    ui.write(e.EV_KEY, e.KEY_P ,1)
-    ui.write(e.EV_KEY, e.KEY_P ,0)
+    ui.write(e.EV_KEY, e.KEY_E ,1)
+    ui.write(e.EV_KEY, e.KEY_E ,0)
     ui.syn()
 
-GPIO.add_event_detect(18, GPIO.FALLING, callback = button18, bouncetime = 500)   
-GPIO.add_event_detect(17, GPIO.FALLING, callback = button17, bouncetime = 500)   
-GPIO.add_event_detect(22, GPIO.FALLING, callback = button22, bouncetime = 500)   
+def button19(channel):
+    ui.write(e.EV_KEY, e.KEY_R ,1)
+    ui.write(e.EV_KEY, e.KEY_R ,0)
+    ui.syn()
+
+def button16(channel):
+    ui.write(e.EV_KEY, e.KEY_1 ,1)
+    ui.write(e.EV_KEY, e.KEY_1 ,0)
+    ui.syn()
+
+def button20(channel):
+    ui.write(e.EV_KEY, e.KEY_2 ,1)
+    ui.write(e.EV_KEY, e.KEY_2 ,0)
+    ui.syn()
+
+def button21(channel):
+    ui.write(e.EV_KEY, e.KEY_3 ,1)
+    ui.write(e.EV_KEY, e.KEY_3 ,0)
+    ui.syn()
+
+GPIO.add_event_detect(5, GPIO.FALLING, callback = button5, bouncetime = 500)   
+GPIO.add_event_detect(6, GPIO.FALLING, callback = button6, bouncetime = 500)   
 GPIO.add_event_detect(13, GPIO.FALLING, callback = button13, bouncetime = 500)   
+GPIO.add_event_detect(19, GPIO.FALLING, callback = button19, bouncetime = 500)   
+
+GPIO.add_event_detect(16, GPIO.FALLING, callback = button16, bouncetime = 500)   
+GPIO.add_event_detect(20, GPIO.FALLING, callback = button20, bouncetime = 500)   
+GPIO.add_event_detect(21, GPIO.FALLING, callback = button21, bouncetime = 500)   
 
 while 1:  
     time.sleep(1)  
