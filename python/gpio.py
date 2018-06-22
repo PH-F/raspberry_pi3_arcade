eb852a73e743dd7338ffe27eb602dee6aef11844
Patrick_Via_Raspberry <raspberry@ph-f.nl>
2018-06-22 10:11:34 +0200

    Update line indent

--- python/gpio.py
+++ python/gpio.py
@@ -4,131 +4,128 @@ from time import sleep
 
 class GPIOhelper:
 
-    def blink(self,pin):
-		time = 0.5
-		message = pin + ":on"
-		
-		pin = int(pin)
-		if pin == 1:
-			pin = 21
-		if pin == 2:
-			pin = 21
-		if pin == 3:
-			pin = 21
-		if pin == 4:
-			pin = 21
+  def blink(self,pin):
+    time = 0.5
+    message = pin + ":on"
+    
+    pin = int(pin)
+    if pin == 1:
+      pin = 21
+    if pin == 2:
+      pin = 21
+    if pin == 3:
+      pin = 21
+    if pin == 4:
+      pin = 21
 
-		if pin in [21,11111,1112,1113]:
-			GPIO.setwarnings(False)
-			GPIO.setmode(GPIO.BCM)
-			GPIO.setup(int(pin), GPIO.OUT, initial=GPIO.HIGH)
-			sleep(float(time));
-			GPIO.output(int(pin), 0)
-		else:
-            message = "pin not found"
+    if pin in [21,11111,1112,1113]:
+      GPIO.setwarnings(False)
+      GPIO.setmode(GPIO.BCM)
+      GPIO.setup(int(pin), GPIO.OUT, initial=GPIO.HIGH)
+      sleep(float(time));
+      GPIO.output(int(pin), 0)
+    else:
+      message = "pin not found"
 
-		GPIO.cleanup()
-		return message
+    GPIO.cleanup()
+    return message
 
-    def animation1(self):
-		time = 0.5
-		message = ""
-        pins = ["21"]
-        pinsReverse = ["21"]
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
+  def animation1(self):
+    time = 0.5
+    message = ""
+    pins = ["21"]
+    pinsReverse = ["21"]
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
 
-        #refactor when the pinnrs are know.
-        for x in pins:
-            GPIO.setup(int(x), GPIO.OUT, initial=GPIO.HIGH)
-            sleep(float(time));
-            GPIO.output(int(x), 0)
+    #refactor when the pinnrs are know.
+    for x in pins:
+      GPIO.setup(int(x), GPIO.OUT, initial=GPIO.HIGH)
+      sleep(float(time));
+      GPIO.output(int(x), 0)
+      for x in pinsReverse:
+        GPIO.setup(int(x), GPIO.OUT, initial=GPIO.HIGH)
+        sleep(float(time));
+        GPIO.output(int(x), 0)
+      GPIO.cleanup()
+    return message
 
-        for x in pinsReverse:
-            GPIO.setup(int(x), GPIO.OUT, initial=GPIO.HIGH)
-            sleep(float(time));
-            GPIO.output(int(x), 0)
+  def animation2(self):
+    time = 0.5
+    message = ""
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
 
-		GPIO.cleanup()
-		return message
+    for x in range(2):
+      GPIO.setup(21, GPIO.OUT, initial=GPIO.HIGH)
+      GPIO.setup(21, GPIO.OUT, initial=GPIO.HIGH)
+      GPIO.setup(21, GPIO.OUT, initial=GPIO.HIGH)
+      GPIO.setup(21, GPIO.OUT, initial=GPIO.HIGH)
+      sleep(float(time));
 
-    def animation2(self):
-		time = 0.5
-		message = ""
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
+      GPIO.output(21, 0)
+      GPIO.output(21, 0)
+      GPIO.output(21, 0)
+      GPIO.output(21, 0)
+      sleep(float(time));
 
-        for x in range(2):
-            GPIO.setup(21, GPIO.OUT, initial=GPIO.HIGH)
-            GPIO.setup(21, GPIO.OUT, initial=GPIO.HIGH)
-            GPIO.setup(21, GPIO.OUT, initial=GPIO.HIGH)
-            GPIO.setup(21, GPIO.OUT, initial=GPIO.HIGH)
-            sleep(float(time));
+    GPIO.cleanup()
+    return message
 
-            GPIO.output(21, 0)
-            GPIO.output(21, 0)
-            GPIO.output(21, 0)
-            GPIO.output(21, 0)
-            sleep(float(time));
+  def switchOn(self,pin):
+    pin = int(pin)
+    message = ""
 
-		GPIO.cleanup()
-		return message
+    if pin == 1:
+      pin = 21
+    if pin == 2:
+      pin = 21
+    if pin == 3:
+      pin = 21
+    if pin == 4:
+      pin = 21
 
-    def switchOn(self,pin):
-		pin = int(pin)
-		message = ""
+    if pin in [21,11111,1112,1113]:
+      GPIO.setwarnings(False)
+      GPIO.setmode(GPIO.BCM)
+      GPIO.setup(int(pin), GPIO.OUT, initial=GPIO.HIGH)
+    else:
+      message = "pin not found"
 
-		if pin == 1:
-			pin = 21
-		if pin == 2:
-			pin = 21
-		if pin == 3:
-			pin = 21
-		if pin == 4:
-			pin = 21
+    GPIO.cleanup()
+    return message
 
-		if pin in [21,11111,1112,1113]:
-			GPIO.setwarnings(False)
-			GPIO.setmode(GPIO.BCM)
-			GPIO.setup(int(pin), GPIO.OUT, initial=GPIO.HIGH)
-		else:
-		  message = "pin not found"
+  def switchOff(self,pin):
+    pin = int(pin)
+    message = ""
 
-		GPIO.cleanup()
-		return message
+    if pin == 1:
+      pin = 21
+    if pin == 2:
+      pin = 21
+    if pin == 3:
+      pin = 21
+    if pin == 4:
+      pin = 21
 
-    def switchOff(self,pin):
-		pin = int(pin)
-		message = ""
+    if pin in [21,11111,1112,1113]:
+      GPIO.setwarnings(False)
+      GPIO.setmode(GPIO.BCM)
+      GPIO.setup(int(pin), GPIO.OUT, initial=GPIO.LOW)
+    else:
+      message = "pin not found"
 
-		if pin == 1:
-			pin = 21
-		if pin == 2:
-			pin = 21
-		if pin == 3:
-			pin = 21
-		if pin == 4:
-			pin = 21
+    GPIO.cleanup()
+    return message
 
-		if pin in [21,11111,1112,1113]:
-			GPIO.setwarnings(False)
-			GPIO.setmode(GPIO.BCM)
-			GPIO.setup(int(pin), GPIO.OUT, initial=GPIO.LOW)
-		else:
-		  message = "pin not found"
+  def reset(self):
+    message = ""
+    pins = ["21"]
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
 
-		GPIO.cleanup()
-		return message
+    for x in pins:
+      GPIO.setup(int(x), GPIO.OUT, initial=GPIO.LOW)
 
-    def reset(self):
-        message = ""
-        pins = ["21"]
-
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-
-        for x in pins:
-            GPIO.setup(int(x), GPIO.OUT, initial=GPIO.LOW)
-
-        GPIO.cleanup()
-        return message
+    GPIO.cleanup()
+    return message
